e4e7aa4ad0a6923a6703d7478c93355d3a60baad
priyalkhare2005-png <priyalkhare2005@gmail.com>
2025-11-30 00:16:24 +0530

    Add XOR encryption function for text
    Implement a simple XOR encryption function.

--- PROJECT 26.c
+++ PROJECT 26.c
@@ -1,1 +1,13 @@
+int secrets(char *text,char *password){    //we want text to be encrypted using password
+    
+    int key=password[0];
 
+
+    int i;
+    //now loop and encrypt the text
+    for(i=0;text[i]!='\0';i++){
+        text[i]^=key;
+    }
+    
+    return 0;
+}
